b0e676019ebdabfe93a1c0b4a076e9e7f888eedf
Nitesh Kumar <niteshsaini1296@gmail.com>
2026-01-30 13:50:19 +0530

    Core Components Added

--- src/Screens/Products/PaymentLinks.jsx
+++ src/Screens/Products/PaymentLinks.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar'
 import PaymentLinksComponent from '../../Components/PaymentLinksComponent'
+import PaymentLinkSupport from '../../Components/PaymentLinkSupport'
+import CryptoCTA from '../../Components/CryptoCTA'
+import SecurityTransparency from '../../Components/SecurityTransparency'
+import Footer from '../../Components/Footer'
 
 const PaymentLinks = () => {
   return (
     <div>
         <Navbar/>
         <PaymentLinksComponent/>
+        <PaymentLinkSupport/>
+        <SecurityTransparency/>
+        <CryptoCTA/>
+        <Footer/>
       
     </div>
   )
